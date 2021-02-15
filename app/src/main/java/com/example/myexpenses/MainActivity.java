66af6804a279f5d5af7c684c04f9581b788d9752
lukasz94w <lukasz94w@wp.pl>
2021-02-15 19:36:36 +0100

    Main window layout

--- app/src/main/java/com/example/myexpenses/MainActivity.java
+++ app/src/main/java/com/example/myexpenses/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.myexpenses;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView headerTextView = (TextView) findViewById(R.id.headerTextView);
+        Button butonEnterExpense = (Button) findViewById(R.id.buttonEnterExpense);
+        Button buttonEnterIncome = (Button) findViewById(R.id.buttonEnterIncome);
+
     }
 }
